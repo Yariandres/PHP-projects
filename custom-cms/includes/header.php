2f0c6147b372872e5eb60001372e17875879cff0
Yari Andres <yari.andres@gmail.com>
2019-12-01 23:02:43 +0100

    blog post databese created

--- custom-cms/includes/header.php
+++ custom-cms/includes/header.php
@@ -38,7 +38,7 @@
           </div>
 
           <div class="col-4 text-center">
-            <a class="blog-header-logo text-dark" href="index.php"></a>
+            <a class="blog-header-logo text-dark" href="#"></a>
           </div>
 
           <div class="col-4 d-flex justify-content-end align-items-center">
@@ -51,20 +51,21 @@
       <div class="nav-scroller py-1 mb-2">
         <nav class="navbar d-flex justify-content-between">
          
-            <?php if(isset($_GET['caterogy'])) { ?>
-            
-              <a class="p-2 text-muted " href="index.php">Home</a>
+            <?php if(isset($_GET['caterogy'])) { ?>            
+              <a class="p-2" href="index.php">Home</a>
             <?php } else {  ?>
-              <a class="p-2 text-muted active" href="index.php">Home</a>
+              <a class="p-2  active" href="index.php">Home</a>
             <?php } ?>
 
             <?php if($categories->num_rows > 0) {
             while($row = $categories->fetch_assoc()) {
-            ?>
-            <a class="p-2 text-muted" href="index.php?category=<?php echo $row['id'] ?>"><?php echo $row['text']; ?></a>
-            <?php  } } ?>
-         
+              if (isset($_GET['category']) && $row['id'] == $_GET['category']) {?>
+              <a class="p-2 active" href="index.php?category=<?php echo $row['id'] ?>"><?php echo $row['text']; ?></a>
+                <?php } else echo "<a class='p-2' href='index.php?category=$row[id]'>$row[text]</a>";
+              } } ?>
+          </ul>      
         </nav>
+        
       </div>
 
       
