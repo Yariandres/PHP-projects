44742b687545a85bbeb0ec3a3a657fc06856b866
Yari Andres <yari.andres@gmail.com>
2019-12-15 22:56:30 +0100

    editing includes/header.php

--- custom-cms/includes/header.php
+++ custom-cms/includes/header.php
@@ -35,7 +35,7 @@
       <header class="blog-header py-3">
         <div class="row flex-nowrap justify-content-between align-items-center">
           <div class="col-4 pt-1">
-            <a class="blog-header-logo text-dark" href="#">Logo</a>
+            <a class="blog-header-logo text-dark" href="index.php">Logo</a>
           </div>
           
           <div class="col-4 d-flex justify-content-end align-items-center">
