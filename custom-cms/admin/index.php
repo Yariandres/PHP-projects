8b6e1ddc53fbfb969edb226e974f6c8a596db8b7
Yari Andres <yari.andres@gmail.com>
2019-12-19 14:15:07 +0100

    modified the dashboard template

--- custom-cms/admin/index.php
+++ custom-cms/admin/index.php
@@ -91,16 +91,15 @@
       </div>
 
 
-      <h2>Section title</h2>
+      <h2>Recent Posts</h2>
       <div class="table-responsive">
         <table class="table table-striped table-sm">
           <thead>
             <tr>
-              <th>#</th>
-              <th>Header</th>
-              <th>Header</th>
-              <th>Header</th>
-              <th>Header</th>
+              <th>Date Posted</th>
+              <th>Title</th>
+              <th>Author</th>
+              <th>Actions</th>
             </tr>
           </thead>
           <tbody>
@@ -108,114 +107,86 @@
               <td>1,001</td>
               <td>Lorem</td>
               <td>ipsum</td>
-              <td>dolor</td>
-              <td>sit</td>
+              <td><a href="#" class="btn btn-warning mr-1">Edit</a><a href="#" class="btn btn-danger">Delete</a></td>
+           
             </tr>
             <tr>
               <td>1,002</td>
               <td>amet</td>
               <td>consectetur</td>
-              <td>adipiscing</td>
-              <td>elit</td>
+              <td><a href="#" class="btn btn-warning mr-1">Edit</a><a href="#" class="btn btn-danger">Delete</a></td>
+            
             </tr>
             <tr>
               <td>1,003</td>
               <td>Integer</td>
               <td>nec</td>
-              <td>odio</td>
-              <td>Praesent</td>
+              <td><a href="#" class="btn btn-warning mr-1">Edit</a><a href="#" class="btn btn-danger">Delete</a></td>
+              
+            </tr>           
+          </tbody>
+        </table>
+
+        <!-- COMENTS TABLE -->
+        <h2>Recent Comments</h2>
+        <table class="table table-striped table-sm">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Comment</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1,001</td>
+              <td>Lorem</td>
+              <td><a href="#" class="btn btn-success mr-1">Approve</a><a href="#" class="btn btn-danger">Delete</a></td>
+           
+            </tr>
+            <tr>
+              <td>1,002</td>
+              <td>amet</td>
+              <td><a href="#" class="btn btn-success mr-1">Approve</a><a href="#" class="btn btn-danger">Delete</a></td>
+            
             </tr>
             <tr>
               <td>1,003</td>
-              <td>libero</td>
-              <td>Sed</td>
-              <td>cursus</td>
-              <td>ante</td>
+              <td>Integer</td>
+              <td><a href="#" class="btn btn-success mr-1">Approve</a><a href="#" class="btn btn-danger">Delete</a></td>
+              
+            </tr>           
+          </tbody>
+        </table>
+
+        <!-- CATEGORY TABLE -->
+        <h2>Recent Categories</h2>
+        <table class="table table-striped table-sm">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Title</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1,001</td>
+              <td>Lorem</td>
+              <td><a href="#" class="btn btn-success mr-1">Approve</a><a href="#" class="btn btn-danger">Delete</a></td>
+           
             </tr>
             <tr>
-              <td>1,004</td>
-              <td>dapibus</td>
-              <td>diam</td>
-              <td>Sed</td>
-              <td>nisi</td>
+              <td>1,002</td>
+              <td>amet</td>
+              <td><a href="#" class="btn btn-success mr-1">Approve</a><a href="#" class="btn btn-danger">Delete</a></td>
+            
             </tr>
             <tr>
-              <td>1,005</td>
-              <td>Nulla</td>
-              <td>quis</td>
-              <td>sem</td>
-              <td>at</td>
-            </tr>
-            <tr>
-              <td>1,006</td>
-              <td>nibh</td>
-              <td>elementum</td>
-              <td>imperdiet</td>
-              <td>Duis</td>
-            </tr>
-            <tr>
-              <td>1,007</td>
-              <td>sagittis</td>
-              <td>ipsum</td>
-              <td>Praesent</td>
-              <td>mauris</td>
-            </tr>
-            <tr>
-              <td>1,008</td>
-              <td>Fusce</td>
-              <td>nec</td>
-              <td>tellus</td>
-              <td>sed</td>
-            </tr>
-            <tr>
-              <td>1,009</td>
-              <td>augue</td>
-              <td>semper</td>
-              <td>porta</td>
-              <td>Mauris</td>
-            </tr>
-            <tr>
-              <td>1,010</td>
-              <td>massa</td>
-              <td>Vestibulum</td>
-              <td>lacinia</td>
-              <td>arcu</td>
-            </tr>
-            <tr>
-              <td>1,011</td>
-              <td>eget</td>
-              <td>nulla</td>
-              <td>Class</td>
-              <td>aptent</td>
-            </tr>
-            <tr>
-              <td>1,012</td>
-              <td>taciti</td>
-              <td>sociosqu</td>
-              <td>ad</td>
-              <td>litora</td>
-            </tr>
-            <tr>
-              <td>1,013</td>
-              <td>torquent</td>
-              <td>per</td>
-              <td>conubia</td>
-              <td>nostra</td>
-            </tr>
-            <tr>
-              <td>1,014</td>
-              <td>per</td>
-              <td>inceptos</td>
-              <td>himenaeos</td>
-              <td>Curabitur</td>
-            </tr>
-            <tr>
-              <td>1,015</td>
-              <td>sodales</td>
-              <td>ligula</td>
-              <td>in</td>
-              <td>libero</td>
-            </tr>
+              <td>1,003</td>
+              <td>Integer</td>
+              <td><a href="#" class="btn btn-success mr-1">Approve</a><a href="#" class="btn btn-danger">Delete</a></td>              
+            </tr>           
           </tbody>
         </table>
       </div>
