e68e907d2ed1fe56713276bbd2a82e58e07839e0
Yari Andres <yari.andres@gmail.com>
2019-12-19 14:25:04 +0100

    split header/side bar in separate file and include them

---
+++ custom-cms/admin/includes/sidebar.php
@@ -0,0 +1,40 @@
+<?php?>
+<div class="container-fluid">
+  <div class="row">
+    <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+      <div class="sidebar-sticky">
+        <ul class="nav flex-column">
+
+          <li class="nav-item">
+            <a class="nav-link active" href="index.php">
+              <span data-feather="home"></span>
+              Dashboard <span class="sr-only">(current)</span>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="posts.php">
+              <span data-feather="file"></span>
+              Posts
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="comments.php">
+              <span data-feather="file"></span>
+              Comments
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="categories.php">
+              <span data-feather="file"></span>
+              Categories
+            </a>
+          </li>
+        </ul> <!-- /nav ul  -->        
+      </div><!-- /sidebar-sticky  -->      
+    </nav> <!-- /nav  -->
+    
+
+    <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
--- custom-cms/admin/index.php
+++ custom-cms/admin/index.php
@@ -1,91 +1,7 @@
-
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Jekyll v3.8.6">
-    <title>Dashboard Template · Bootstrap</title>    
-
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.css" rel="stylesheet">
-  
-
-
-
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-    </style>
-
-    <!-- Custom styles for this template -->
-    <link href="css/dashboard.css" rel="stylesheet">
-  </head>
-  <body>
-    <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="index.php">Baby Wearing</a>
-
-      <ul class="navbar-nav px-3">
-        <li class="nav-item text-nowrap">
-          <a class="nav-link" href="logout.php">Sign out</a>
-        </li>
-      </ul>
-
-    </nav>
-
-<div class="container-fluid">
-  <div class="row">
-    <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-      <div class="sidebar-sticky">
-        <ul class="nav flex-column">
-
-          <li class="nav-item">
-            <a class="nav-link active" href="index.php">
-              <span data-feather="home"></span>
-              Dashboard <span class="sr-only">(current)</span>
-            </a>
-          </li>
-
-          <li class="nav-item">
-            <a class="nav-link" href="posts.php">
-              <span data-feather="file"></span>
-              Posts
-            </a>
-          </li>
-
-          <li class="nav-item">
-            <a class="nav-link" href="comments.php">
-              <span data-feather="file"></span>
-              Comments
-            </a>
-          </li>
-
-          <li class="nav-item">
-            <a class="nav-link" href="categories.php">
-              <span data-feather="file"></span>
-              Categories
-            </a>
-          </li>
-        </ul> <!-- /nav ul  -->        
-      </div><!-- /sidebar-sticky  -->      
-    </nav> <!-- /nav  -->
-    
-
-    <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+<?php 
+include("includes/header.php");
+include("includes/sidebar.php");
+?>
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Dashboard</h1>        
       </div>
@@ -173,19 +89,19 @@
             <tr>
               <td>1,001</td>
               <td>Lorem</td>
-              <td><a href="#" class="btn btn-success mr-1">Approve</a><a href="#" class="btn btn-danger">Delete</a></td>
+              <td><a href="#" class="btn btn-warning mr-1">Edit</a><a href="#" class="btn btn-danger">Delete</a></td>
            
             </tr>
             <tr>
               <td>1,002</td>
               <td>amet</td>
-              <td><a href="#" class="btn btn-success mr-1">Approve</a><a href="#" class="btn btn-danger">Delete</a></td>
+              <td><a href="#" class="btn btn-warning mr-1">Edit</a><a href="#" class="btn btn-danger">Delete</a></td>
             
             </tr>
             <tr>
               <td>1,003</td>
               <td>Integer</td>
-              <td><a href="#" class="btn btn-success mr-1">Approve</a><a href="#" class="btn btn-danger">Delete</a></td>              
+              <td><a href="#" class="btn btn-warning mr-1">Edit</a><a href="#" class="btn btn-danger">Delete</a></td>              
             </tr>           
           </tbody>
         </table>
