d5ad8cc93e91e00f691c6454f246c757096bd91a
Yari Andres <yari.andres@gmail.com>
2019-12-22 19:59:56 +0100

    admin sign in sign out

--- custom-cms/admin/signin.php
+++ custom-cms/admin/signin.php
@@ -1,3 +1,25 @@
+<?php 
+session_start();
+
+include_once "includes/config.php";
+include_once "includes/db.php";
+
+if (isset($_POST['login'])) {
+  $email = mysqli_real_escape_string($db , $_POST['email']);
+  $password = mysqli_real_escape_string($db , $_POST['password']);
+
+  $query = "SELECT * FROM admin WHERE email='$email' AND password='$password' ";
+  $result = $db->query($query);
+
+  if($result->num_rows == 1) {
+    $_SESSION['email'] = $email;
+    header("Location:index.php");
+    exit();
+  }
+
+}
+
+?>
 
 <!doctype html>
 <html lang="en">
@@ -34,16 +56,18 @@
 
   </head>
   <body class="text-center">
-      <form class="form-signin">
+
+      <form class="form-signin" method="POST">
         <img class="mb-4" src="" alt="" width="72" height="72">
-        <h1 class="h3 mb-3 font-weight-normal">Welcome back! Sign in?</h1>
+        <h1 class="h3 mb-3 font-weight-normal">Sign in?</h1>
+
         <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <input type="email" id="inputEmail" name="email" class="form-control" placeholder="Email address" required autofocus>
         <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
         
         
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      <button class="btn btn-lg btn-primary btn-block" name="login" type="submit">Sign in</button>
 
       <p class="mt-5 mb-3 text-muted" id="date"></p>
   </form>
